11ef3f723edf8dcbe3516a36ed688ab5ea60a337
amy <ctrlalternativedelete@gmail.com>
2022-05-23 13:27:49 +1000

    fixed caching implementation

--- src/modules/reddit.ts
+++ src/modules/reddit.ts
@@ -6,30 +6,55 @@ const subreddits: string[] = [
     "yuri_jp",
     "yurification"
 ];
-const cache: string[] = [];
 
 export async function getRandomImage() {
     return (new Promise<Omit<Submission, "then"> | undefined>(async resolve => {
+        const history = (await clients.RedditClient.getMe().getUpvotedContent()).flatMap(e => e.id);
+
         let listing: Submission[] = [];
+        
         for (const sub of subreddits) {
-            const tempsublisting = (await clients.RedditClient.getSubreddit(sub).getHot({limit: 75})).filter(e => {
-                return (
+            const tempsublisting = (await clients.RedditClient.getSubreddit(sub).getHot({limit: 100})).filter(e => {
+                return Boolean(
                     !e.over_18 && 
                     e.url && 
                     e.thumbnail !== "self" &&
                     e.thumbnail !== "" && 
-                    !cache.includes(e.url)
+                    !history.includes(e.id) &&
+                    !arrayHasDuplicates(getParents(e)?.flatMap(e => e.id).concat(history))
                 );
             });
             listing = listing.concat(tempsublisting);
         }
         const returnval = listing.at(randomIntFromInterval(0, listing.length - 1))
-        if (returnval) { cache.push(returnval.url); }
+
+        // upvote for history
+        if (returnval) { 
+            await returnval.upvote().then(() => { return; }); 
+            const returnvalparents = getParents(returnval);
+            if (returnvalparents) {
+                for (const parent of returnvalparents) {
+                    await parent.upvote().then(() => { return; });
+                }
+            }
+        }
         resolve(returnval);
     }));
 }
 
+function getParents(s: Submission) {
+    return (s as any).crosspost_parent_list as Submission[] | undefined;
+}
 
+
+function arrayHasDuplicates(array: Array<any> | undefined) {
+    if (array) {
+        return (new Set(array)).size !== array.length;
+    }   
+    else {
+        return false;
+    }
+}
 
 function randomIntFromInterval(min: number, max: number) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
